ece76d03ef812485e9ae6d9b002b669af5014245
Ken Gullaksen <ken.gullaksen@embriq.no>
2014-02-28 18:10:32 +0100

    fixed e2e test

--- e2e-test/spec.js
+++ e2e-test/spec.js
@@ -1,10 +1,11 @@
 describe('template', function() {
   it('should expose the templates to __json__', function() {
-    json = __json__['example.json'];
-    jsObj = JSON.parse json
-    expect(jsObj).toBeDefined();
-    expect(jsObj.name).toBe('foo');
-    expect(jsObj.friends.length).toBe(1);
-    expect(jsObj.friends[0].name).toBe('bar');
+    example = __json__['example.json']
+    expect(example).toBeDefined()
+    jsObj = JSON.parse(example)
+    expect(jsObj).toBeDefined()
+    expect(jsObj.name).toBe('foo')
+    expect(jsObj.friends.length).toBe(1)
+    expect(jsObj.friends[0].name).toBe('bar')
   })
 })
